9034bc12d0cd2d677ec59a3ae6d9dbb1429225a9
Kirill Berezin <berezk7@gmail.com>
2023-09-09 18:59:13 +0400

    Добавьте файлы проекта.

---
+++ Client/Client.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main()
+    {
+        // IP-адрес и порт сервера
+        string serverIP = "127.0.0.1";
+        int serverPort = 1111;
+
+        try
+        {
+            // Создание объекта TcpClient для подключения к серверу
+            TcpClient client = new TcpClient(serverIP, serverPort);
+            Console.Write("Connected to server.\n");
+
+            // Поток для чтения и записи данных
+            NetworkStream stream = client.GetStream();
+
+            while (true)
+            {
+                // Ввод сообщения
+                Console.Write("Enter a message (to close connection, enter 'exit'): ");
+                string message = Console.ReadLine();
+
+                // Отправка сообщения серверу
+                byte[] messageBuffer = Encoding.UTF8.GetBytes(message);
+                stream.Write(messageBuffer, 0, messageBuffer.Length);
+                Console.Write("Message sent.\n");
+
+                // Буфер для хранения полученного ответа
+                messageBuffer = new byte[1024];
+
+                // Получение ответа от сервера
+                int bytesRead = stream.Read(messageBuffer, 0, messageBuffer.Length);
+                string response = Encoding.UTF8.GetString(messageBuffer, 0, bytesRead);
+                Console.Write("Server reply: " + response + "\n");
+
+                // Проверка условия завершения соединения
+                if (message.ToLower() == "exit")
+                {
+                    break;
+                }
+            }
+
+            // Закрываем подключение
+            client.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,68 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main()
+    {
+        // IP-адрес и порт для сервера
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        int port = 1111;
+
+        // Создание объекта TcpListener для прослушивания входящих подключений
+        TcpListener server = new TcpListener(ipAddress, port);
+
+        try
+        {
+            // Запуск прослушивания
+            server.Start();
+            Console.Write("Server is running. Waiting for client...\n");
+
+            while (true)
+            {
+                // Принятие входящего подключения
+                TcpClient client = server.AcceptTcpClient();
+                Console.Write("Client connected\n");
+
+                // Поток для чтения и записи данных
+                NetworkStream stream = client.GetStream();
+
+                // Буфер для хранения полученных данных
+                byte[] messageBuffer = new byte[1024];  
+
+                while (true)
+                {
+                    // Чтение данных из потока
+                    int bytesRead = stream.Read(messageBuffer, 0, messageBuffer.Length);
+
+                    // Преобразование полученных данных в строку
+                    string message = Encoding.UTF8.GetString(messageBuffer, 0, bytesRead);
+                    Console.Write("Message received: " + message + "\n\n");
+
+                    // Отправка ответа клиенту
+                    byte[] reply = Encoding.UTF8.GetBytes("Message received");
+                    stream.Write(reply, 0, reply.Length);
+
+                    // Проверка условия завершения соединения
+                    if (message.ToLower() == "exit")
+                    {
+                        break;
+                    }
+                }
+
+                // Закрытие подключения
+                client.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            // Остановка сервера
+            server.Stop();
+        }
+    }
+}
